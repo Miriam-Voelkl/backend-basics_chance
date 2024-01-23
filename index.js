3d2b6fe6e896cfb23868efd03b2b982cc186b6ef
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-23 12:02:39 +0100

    build server that responds with random string

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
